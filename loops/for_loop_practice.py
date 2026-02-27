ed9ec7ffea60d8833735c418ce502a2cc9a1fd30
shridhartapakire <shridhartapakire4051@gmail.com>
2026-02-27 23:46:20 +0530

    Added for loop practice

---
+++ loops/for_loop_practice.py
@@ -0,0 +1,11 @@
+# for_loop_practice.py
+# Python Foundation – Loops
+
+# Print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
+
+# Print even numbers from 1 to 20
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
