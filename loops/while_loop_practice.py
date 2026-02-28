7b556882694d9500003e400c19556804c9cc6ea9
shridhartapakire <shridhartapakire4051@gmail.com>
2026-02-28 22:10:22 +0530

    Added while loop practice

---
+++ loops/while_loop_practice.py
@@ -0,0 +1,21 @@
+# while_loop_practice.py
+# Python Foundation – While Loop Practice
+
+# Print numbers from 1 to 10
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# Print numbers from 10 to 1
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+# Print even numbers from 1 to 20
+i = 1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+    i += 1
